1d87100df0b65362f1fac38b97e625ee34ede9ea
andasan <andasan@gmail.com>
2021-05-26 11:24:00 -0700

    init commit

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+
+const keys = require('../config/keys')
+const User = mongoose.model('User')
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/api/auth/google/callback',
+    proxy: true
+}, async(accessToken, refreshToken, profile, done) => {
+    console.log(accessToken)
+    console.log(refreshToken)
+    console.log(profile)
+}))
